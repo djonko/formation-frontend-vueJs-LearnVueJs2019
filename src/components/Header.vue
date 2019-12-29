ebe07648c8b47f6b1a4070971523826216254f93
olarila-l635 <prevostdjonko@yahoo.fr>
2019-12-29 03:24:30 -0500

    quiz project

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+      <b-nav-item disabled>Counter: {{ innerNumCorrect }}/{{ innerNumTotal }}</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    innerNumCorrect: Number,
+    innerNumTotal: Number,
+  },
+  methods: {},
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
-import App from './App.vue'
 
+import BootstrapVue from 'bootstrap-vue'
+//import './plugins/bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import App from './App.vue'
+Vue.use(BootstrapVue); // for loading  bootstrap js 
 Vue.config.productionTip = false
 
 new Vue({
